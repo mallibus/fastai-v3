bd833a09097d0dae4dc56c2e9d3adf7346a23485
Marcello Morchio <45511108+mallibus@users.noreply.github.com>
2019-06-22 00:25:48 +0200

    Updated with cleaned dataset (google dwl)

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1s411VxjZ6NDM4BGfPyPenShpgwvYTt8n'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1nkJnetnIDdpL9ERjgIbNX47w5-mboR2h'
 export_file_name = 'export.pkl'
 
 classes = ['daitarn3', 'goldrake', 'icub', 'jeeg', 'mazinga', 'tekkaman']
